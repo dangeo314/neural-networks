90ec3bba1c6207d161f1557e02c7d1b121ce38df
Daniel George <dan@Daniels-MacBook-Pro.local>
2020-10-24 16:56:38 -0700

    changed learning parameters

--- src/tester.py
+++ src/tester.py
@@ -3,11 +3,11 @@ import matrixNetwork
 import time
 
 
-n = matrixNetwork.Network((784, 13, 10))
+n = matrixNetwork.Network((784, 100, 10))
 (training_data, test_data) = mnist_loader.load_data()
 
 start = time.time()
 a, f = training_data[0]
-n.chooseMiniBatch(training_data, 40, 1000, 2.0, test_data = test_data)
+n.chooseMiniBatch(training_data, 40, 100,  4.0, test_data = test_data)
 end = time.time()
 
