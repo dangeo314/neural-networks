d64a9904843e7f75cbd89959643af247710c1686
Daniel George <dan@Daniels-MacBook-Pro.local>
2020-10-24 15:50:19 -0700

    changed batch size and learning rate

--- src/tester.py
+++ src/tester.py
@@ -1,4 +1,3 @@
-
 import mnist_loader
 import matrixNetwork
 import time
@@ -9,6 +8,6 @@ n = matrixNetwork.Network((784, 13, 10))
 
 start = time.time()
 a, f = training_data[0]
-n.chooseMiniBatch(training_data, 6, 100, 1.0, test_data = test_data)
+n.chooseMiniBatch(training_data, 40, 1000, 2.0, test_data = test_data)
 end = time.time()
 
